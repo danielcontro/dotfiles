94d81a0e892083527bf80299de05825742239e1b
Daniel Eduardo Contro <dani.contro2@gmail.com>
2024-01-02 17:29:50 +0100

    Better neovim aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -4,5 +4,5 @@ alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
 
-alias lzvim="NVIM_APPNAME=nvim/LazyVim nvim"
-alias unvim="NVIM_APPNAME=nvim/user nvim"
+alias lvim="NVIM_APPNAME=nvims/LazyVim nvim"
+alias uvim="NVIM_APPNAME=nvims/user nvim"
