db8f4ccb36eb38f9007f96d1708565904971d595
Daniel Eduardo Contro <dani.contro2@gmail.com>
2024-01-02 18:01:38 +0100

    Separation of neovim configurations for better stow management

---
+++ LazyVim/.config/nvims/LazyVim/lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+    },
+  },
+}
